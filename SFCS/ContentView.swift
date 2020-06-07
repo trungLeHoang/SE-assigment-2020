67bf953402012575fad56576180426b76b80b86a
Quân <quan260402@gmail.com>
2020-06-07 11:02:47 +0700

    Initial Commit

---
+++ SFCS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SFCS
+//
+//  Created by Đặng Nhật Quân on 6/7/20.
+//  Copyright © 2020 Đặng Nhật Quân. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

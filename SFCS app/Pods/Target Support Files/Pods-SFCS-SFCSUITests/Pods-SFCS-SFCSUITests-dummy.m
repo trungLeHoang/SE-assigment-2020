90bd7bc38fc60dcabb95d451fad783413f0bb3a0
quentintheastronaut <quan260402@gmail.com>
2020-07-26 16:01:03 +0700

    fix remember order and account

--- SFCS app/Pods/Target Support Files/Pods-SFCS-SFCSUITests/Pods-SFCS-SFCSUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SFCS_SFCSUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SFCS_SFCSUITests
-@end
--- SFCS app/Pods/Target Support Files/Pods-SFCS/Pods-SFCS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SFCS : NSObject
-@end
-@implementation PodsDummy_Pods_SFCS
-@end
